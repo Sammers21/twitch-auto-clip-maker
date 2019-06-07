cf3101382375e4930f3694e066ee20a35b678e80
Sammers21 <titantins@gmail.com>
2019-06-07 23:33:20 +0300

    implement chat messages logging

--- src/main/scala/io/github/Sammers21/twitch/Main.scala
+++ src/main/scala/io/github/Sammers21/twitch/Main.scala
@@ -1,9 +1,29 @@
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential
 import com.github.twitch4j.TwitchClientBuilder
+import com.github.twitch4j.chat.TwitchChat
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
 
 object Main extends App {
-  println("Hello world")
+
+
+  private val token: String = args(0)
+  private val log = LoggerFactory.getLogger("Main")
+
+  // chat credential
+  val credential = new OAuth2Credential("twitch", token)
+
+  // twitch client
   val twitchClient = TwitchClientBuilder.builder()
-    .withEnableHelix(true)
+    .withEnableChat(true)
+    .withChatAccount(credential)
     .build()
-  twitchClient.getChat.joinChannel("swifty");
+
+  val chat: TwitchChat = twitchClient.getChat
+  chat.joinChannel("starladder5")
+  private val value: Flux[ChannelMessageEvent] = chat.getEventManager.onEvent(classOf[ChannelMessageEvent])
+  value.subscribe(ok => {
+    log.info(ok.getMessage)
+  })
 }
