345573a770e56ee4c007fe94845008ec8a429c0c
Sammers21 <titantins@gmail.com>
2019-10-16 21:46:19 +0300

    update front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=vb3b4l61t5i2af2svkksagbxuxm26x&redirect_uri=http://clip-maker.com&scope=user:read:email">Connect With Twitch!</a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
