3b7e3d120a2d90c75c7239c4429c5ac62949fe35
Sammers21 <titantins@gmail.com>
2019-10-20 20:05:45 +0300

    cache gradle files when building a docker image

--- http-server/src/main/java/io/github/sammers21/tacm/server/Server.java
+++ http-server/src/main/java/io/github/sammers21/tacm/server/Server.java
@@ -3,8 +3,11 @@ package io.github.sammers21.tacm.server;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.handler.StaticHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
+    private static final Logger log = LoggerFactory.getLogger(Server.class);
 
     private final String name = "twitch-key";
     private final Vertx vertx;
@@ -18,11 +21,12 @@ public class Server {
     public void start() {
         Router router = Router.router(vertx);
         router.route().handler(event -> {
-
+            event.next();
         });
         router.route().handler(StaticHandler.create());
         vertx.createHttpServer()
                 .requestHandler(router)
                 .listen(port);
+        log.info("Started on port:{}", port);
     }
 }
