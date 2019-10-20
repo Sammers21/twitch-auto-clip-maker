fe10e68717522128317499765a48a023fd15f15e
Sammers21 <titantins@gmail.com>
2019-10-20 20:59:18 +0300

    update sign-in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './twitch-button.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=vb3b4l61t5i2af2svkksagbxuxm26x&redirect_uri=http://clip-maker.com&scope=user:read:email">Connect With Twitch!</a>
+                <div id="sign-in-window">
+                    <p>Sign-in</p>
+                    <a href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=vb3b4l61t5i2af2svkksagbxuxm26x&redirect_uri=http://clip-maker.com&scope=user:read:email">
+                        <button id="twitch-button" type="button" className="btn btn-twitch">
+                            <p id="twitch-login-text">Twitch</p>
+                        </button>
+                    </a>
+                </div>
             </header>
         </div>
     );
