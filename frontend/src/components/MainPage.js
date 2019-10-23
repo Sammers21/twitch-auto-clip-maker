2b991216c31ae93a239e88732c8634a8425db8d5
Sammers21 <titantins@gmail.com>
2019-10-23 21:41:43 +0300

    update css

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -12,7 +12,8 @@ class MainPage extends React.Component {
             refresh_token: Cookies.get("refresh_token"),
             twitch_scope: Cookies.get("twitch_scope"),
             username: 'USER_NAME',
-            email: 'EMAIL'
+            email: 'EMAIL',
+            profile_image_url: "/favicon.ico"
         };
         this.loadState();
     }
@@ -28,14 +29,23 @@ class MainPage extends React.Component {
                     {
                         ...this.state,
                         username: response.data.data[0].login,
-                        email: response.data.data[0].email
+                        email: response.data.data[0].email,
+                        profile_image_url: response.data.data[0].profile_image_url
                     })
             })
     }
 
     render() {
         return (
-            <div>
+            <div id="main-page">
+                <nav class="border-colored ">
+                    <ol>
+                        <li class="nav-elem border-colored "><img id="profile-img" src={this.state.profile_image_url}
+                                                                  alt="Profile pic"/></li>
+                        <li class="nav-elem border-colored "><p>{this.state.username}</p></li>
+                        <li class="nav-elem border-colored "><p>{this.state.email}</p></li>
+                    </ol>
+                </nav>
                 <h3>ACCESS_TOKEN={this.state.access_token}</h3>
                 <h3>REFRESH_TOKEN={this.state.refresh_token}</h3>
                 <h3>SCOPE={this.state.twitch_scope}</h3>
