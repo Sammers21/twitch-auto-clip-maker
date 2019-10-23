72ea7cd8e4f7af3b51827e6c638ef0d0e735b7b1
Sammers21 <titantins@gmail.com>
2019-10-23 10:29:07 +0300

    add js cookie dependency and use it to show cookies

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Cookies from 'js-cookie'
+import './MainPage.css'
 
 function MainPage() {
     return (
-        <p>YOU ARE LOGGED IN</p>
+        <div>
+            <h3>ACCESS_TOKEN={Cookies.get("access_token")}</h3>
+            <h3>REFRESH_TOKEN={Cookies.get("refresh_token")}</h3>
+            <h3>SCOPE={Cookies.get("twitch_scope")}</h3>
+        </div>
     );
 }
 
